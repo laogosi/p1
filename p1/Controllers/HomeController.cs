ced1619bb95bb41c49e555d76e6de86a215ce852
laogosi <uhimcukmarina97@gmail.com>
2024-10-22 00:39:37 +0300

    Добавьте файлы проекта.

---
+++ p1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.StudentName = "Юхимчук Марина";
+            ViewBag.GroupNumber = "ІПЗ-22-1";
+            ViewBag.University = "Хмельницький національний університет";
+            ViewBag.ProjectTopic = "Магазин квітів";
+            return View();
+        }
+    }
+}
